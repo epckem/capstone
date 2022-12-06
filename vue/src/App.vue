8cb0e07ba7aff807485c269a112d12ef01c20f50
Mitch Epcke <epckem@gmail.com>
2022-12-06 11:32:14 -0500

    login page done

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link id="home-btn" v-bind:to="{ name: 'home' }">Home</router-link
+      >&nbsp;|&nbsp;
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
 </template>
+<style scoped>
+#home-btn {
+  color: white;
+  text-decoration: none;
+  font-size: x-large;
+}
+</style>
