a042bb25662e3243b15334155edc1b359547719a
Mitch Epcke <epckem@gmail.com>
2022-12-07 11:30:47 -0500

    linked restaurantslist to home

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">
+    <restaurants-list> </restaurants-list>
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
 </template>
 
 <script>
+import RestaurantsList from "../components/RestaurantsList.vue";
+
 export default {
-  name: "home"
+  components: {
+    RestaurantsList,
+  },
 };
 </script>
+<style scoped>
+h1,
+p {
+  color: red;
+  font-size: xx-large;
+}
+</style>
