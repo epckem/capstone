8a2341deef4bf6a3b38b23c6d72322810c16aa18
Mitch Epcke <epckem@gmail.com>
2022-12-07 12:47:34 -0500

    invitation form almost done

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,6 +3,9 @@
     <div id="nav">
       <router-link id="home-btn" v-bind:to="{ name: 'home' }">Home</router-link
       >&nbsp;|&nbsp;
+      <router-link id="invite-btn" v-bind:to="{ name: 'InviteView' }"
+        >Invite</router-link
+      >&nbsp;|&nbsp;
       <router-link
         v-bind:to="{ name: 'logout' }"
         v-if="$store.state.token != ''"
---
+++ vue/src/views/InviteView.vue
@@ -0,0 +1,16 @@
+<template>
+  <Invite></Invite>
+</template>
+
+<script>
+import Invite from "../components/Invite.vue";
+export default {
+  name: "InviteView",
+  components: {
+    Invite,
+  },
+};
+</script>
+
+<style>
+</style>
