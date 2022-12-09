3fb6273171e5d26a5d50f94feda892d22a94803b
Mazie <eizam101@gmail.com>
2022-12-09 16:23:39 -0500

    Restaurant Detail Page Update

--- vue/src/components/RestaurantDetails.vue
+++ vue/src/components/RestaurantDetails.vue
@@ -1,34 +1,55 @@
 <template>
   <div class="details">
-    <!-- <div>
-      <img
-        src="https://i.pinimg.com/originals/6d/00/9e/6d009e1b243cc054596b94082499e2ce.png"
-        class="ratingStar"
-      />
-    </div>
-    <div>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg"
-        alt="restaurant image"
-      />
-    </div>
-    <div>
-      <p>Mexican</p>
-      <p>123 Main Street, Cincinnati, OH 45212</p>
-      <p>M-F: 11am-11pm</p>
-      <span>Open</span>
-    </div> -->
-
     <h1>{{ restaurant.name }}</h1>
+
+    <div>
+      <div class="rating">
+        <img
+          src="../assets/star.png"
+          v-bind:title="restaurant.rating + ' Star Review'"
+          class="ratingStar"
+          v-for="n in Math.ceil(restaurant.rating)"
+          v-bind:key="n"
+        />
+      </div>
+
+      <h3>{{ restaurant.type }}</h3>
+      <p>{{ restaurant.description }}</p>
+      <p>
+        {{ restaurant.address }} {{ restaurant.city }}, {{ restaurant.state }} -
+        {{ restaurant.zipcode }}
+      </p>
+    </div>
+    <div>
+      <span>Call To Order: {{ restaurant.phoneNumber }}</span>
+      <div>
+        <h4>Hours:</h4>
+        <p>
+          {{ convertTime(restaurant.open) }} -
+          {{ convertTime(restaurant.close) }}
+        </p>
+      </div>
+      <div>
+        <h4>Services:</h4>
+        <p>Delivery</p>
+        <p>Dine-in</p>
+        <p>Carry-out</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import RestaurantService from "../services/RestaurantService";
+import moment from "moment";
 
 export default {
   name: "restaurant-details",
-
+  methods: {
+    convertTime(time) {
+      return moment(time, "HH:mm:ss").format("h:mm A");
+    },
+  },
   data() {
     return {
       restaurant: {},
