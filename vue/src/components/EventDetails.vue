f9937925f0839e5c407308f8a6dcc02bfb02298a
Ray Green <rayshell.l.green@gmail.com>
2022-12-13 15:39:04 -0500

    event details

---
+++ vue/src/components/EventDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import EventService from "../services/EventService"
+
+export default {
+  name: "event-detail",
+  props: [],
+  beforeCreate() {
+    document.body.className = "event-details";
+  },
+  data(){
+    return{
+      event: {},
+    };
+  },
+  created(){
+    const id = parseInt(this.$route.params.id);
+    EventService.getEvent(id).then((event) => {
+        this.event = event.data 
+    });
+  },
+
+}
+</script>
+
+<style>
+
+</style>
